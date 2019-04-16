d9bafd7ef7f7643f39669af7c6ee2431c89c9d9c
Max <hubs.maxwell@gmail.com>
2019-04-16 19:51:13 -0400

    IU-2019.1 <hubsm@DESKTOP-JTCUENN Create IntelliLang.xml, ide.general.xml, git.xml, Custom.icls, project.default.xml, diff.xml, Default copy.xml, Default.xml, applicationLibraries.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, keymap.xml, gradle.run.settings.xml, colors.scheme.xml, scala_config.xml, Darcula copy.icls, github.xml, mavenVersion.xml, Default.xml, hg.xml, scala.xml, laf.xml, parameter.hints.xml, jdk.table.xml, path.macros.xml, web-browsers.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**
+ * ***********************************************************************
+ * Copyright Max Hubbard (c) 2015. All Rights Reserved.
+ * Any code contained within this document, and any associated documents with similar branding
+ * are the sole property of Max. Distribution, reproduction, taking snippets, or
+ * claiming any contents as your own will break the terms of the license, and void any
+ * agreements with you, the third party.
+ * ************************************************************************
+ */
